f7b7aca0889273438436b48be16d2ce5f5190b0d
halinh <halinh1112@yahoo.com>
2020-10-27 17:01:52 +0000

    modify travis.yml

---
+++ app/app/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+from .cacl import add, substract
+
+class CaclTest(TestCase):
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        self.assertEqual(add(3,8), 11)
+
+    def test_substract_numbers(self):
+        """Test that two numbers are substracted and returned"""
+        self.assertEqual(substract(5,11), 6)
